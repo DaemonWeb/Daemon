143712269c1ba5ff6695559ca7d20633c47e7c8a
Josh Elias <josh@daemontech.tools>
2023-01-25 10:03:28 -0500

    Fix Tailwind config

--- apps/portfolio-website/tailwind.config.js
+++ apps/portfolio-website/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{html,js,jsx,tsx}",
-    "./components/**/*.{html,js,jsx,tsx}"
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
