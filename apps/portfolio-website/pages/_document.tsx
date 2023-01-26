b15f07d127994da2bbe136886aebe24da1f84c4d
Josh Elias <josh@daemontech.tools>
2023-01-26 09:40:57 -0500

    Move favicon and viewpart meta tags to document

--- apps/portfolio-website/pages/_document.tsx
+++ apps/portfolio-website/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en" className='h-full bg-gray-100'>
-      <Head />
+      <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className="h-full">
         <Main />
         <NextScript />
