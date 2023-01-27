9952c9008117f50debb16fed8d34dba1dd2c89fa
Josh Elias <josh@daemontech.tools>
2023-01-27 14:52:51 -0500

    Add FeatureSection to Home page

--- apps/portfolio-website/pages/index.tsx
+++ apps/portfolio-website/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { ContentWrapper } from '@/components/ContentView'
 import HomeHero from '@/components/HomeHero'
+import FeatureSection from '@/components/FeatureSection'
 
 
 export default function Home() {
@@ -10,9 +11,8 @@ export default function Home() {
         <title>Daemon Web Tools</title>
         <meta name="description" content="Services, Projects and Portfolio for Daemon Web Tools" />
       </Head>
-
-          <HomeHero />
-      
+      <HomeHero />
+      <FeatureSection />
     </>
   )
 }
