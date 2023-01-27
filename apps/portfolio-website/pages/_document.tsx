4231b82a8782a23e948ed51b8c71076f082b3418
Josh Elias <josh@daemontech.tools>
2023-01-27 11:41:17 -0500

    Remove meta tags from _document Head component

--- apps/portfolio-website/pages/_document.tsx
+++ apps/portfolio-website/pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en" className='h-full bg-gray-100'>
       <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
       </Head>
       <body className="h-full">
