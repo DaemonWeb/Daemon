f9fd2dc67759488f1c56410d61d614698d604509
Josh Elias <josh@daemontech.tools>
2023-01-31 09:52:00 -0500

    Fix unused project prop by only adding title for now

--- apps/portfolio-website/components/Projects/ProjectDescription.tsx
+++ apps/portfolio-website/components/Projects/ProjectDescription.tsx
@@ -2,16 +2,16 @@ import { PaperClipIcon } from '@heroicons/react/20/solid';
 import { Project } from '@/lib/types';
 
 
-export default function ProjectDescription(/*{
+export default function ProjectDescription({
     project,
 }: {
     project: Project,
-}*/) {
+}) {
   return (
     <div className="overflow-hidden bg-white shadow sm:rounded-lg">
       <div className="px-4 py-5 sm:px-6">
         <h3 className="text-lg font-medium leading-6 text-gray-900">Applicant Information</h3>
-        <p className="mt-1 max-w-2xl text-sm text-gray-500">Personal details and application.</p>
+        <p className="mt-1 max-w-2xl text-sm text-gray-500">{project.title}</p>
       </div>
       <div className="border-t border-gray-200">
         <dl>
