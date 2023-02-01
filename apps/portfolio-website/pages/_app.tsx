ec82d89f4f9959d3c045c465c2f3bf2005aaee7d
Josh Elias <josh@daemontech.tools>
2023-02-01 10:08:56 -0500

    Change default font to Nunito

--- apps/portfolio-website/pages/_app.tsx
+++ apps/portfolio-website/pages/_app.tsx
@@ -1,11 +1,18 @@
 import '@/styles/globals.css';
+import { Nunito } from '@next/font/google';
 import type { AppProps } from 'next/app';
 import Layout from '@/components/Layout';
 
+
+const nunito = Nunito({subsets: ["latin"]});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
+    <div className={nunito.className}>
+      <Layout>
       <Component {...pageProps} />
     </Layout>
+    </div>
+    
   );
 }
