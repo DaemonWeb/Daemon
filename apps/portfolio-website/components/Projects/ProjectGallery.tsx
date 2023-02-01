b1d4df9f74de59887038daa78ec98275e5f8ca17
Josh Elias <josh@daemontech.tools>
2023-02-01 10:55:05 -0500

    Play with Tailwind Grid Flow

--- apps/portfolio-website/components/Projects/ProjectGallery.tsx
+++ apps/portfolio-website/components/Projects/ProjectGallery.tsx
@@ -1,4 +1,5 @@
 import Image, { StaticImageData } from "next/image";
+import { ContentContainer } from "../ContentView";
 
 export default function ProjectGallery({
   images
@@ -6,24 +7,20 @@ export default function ProjectGallery({
   images: StaticImageData[]
 }) {
     return (
-      <div className="overflow-hidden text-gray-700 ">
-        <div className="container px-5 py-2 mx-auto lg:pt-12 lg:px-32">
-          <div className="flex flex-wrap -m-1 md:-m-2">
-            {images.map((image, i) => (
-              <div key={i} className="flex flex-wrap w-1/3">
-                <div className="w-full p-1 md:p-2 overflow-hidden">
-                  <Image 
-                    alt="gallery" 
-                    className="block object-cover w-full h-auto rounded-lg overflow-hidden"
-                    src={image}
-                    width={300}
-                    height={169}
-                  />
-                </div>
+        <div className="grid grid-flow-row-col-dense grid-cols-3 grid-rows-3">
+          {images.map((image, i) => (
+     
+              <div key={i} className="w-full p-1 md:p-2 overflow-hidden">
+                <Image 
+                  alt="gallery" 
+                  className="object-cover w-full h-auto rounded-lg overflow-hidden"
+                  src={image}
+                  width={300}
+                  height={169}
+                />
               </div>
-            ))}
-          </div>
+            
+          ))}
         </div>
-      </div>
     );
 }
