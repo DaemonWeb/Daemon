a960f6b77a9e6eaf644205a912b114d539f951ae
Josh Elias <josh@daemontech.tools>
2023-02-03 14:54:35 -0500

    Add meta data to Profile Page

--- apps/portfolio-website/pages/profile.tsx
+++ apps/portfolio-website/pages/profile.tsx
@@ -8,6 +8,7 @@ import linkedinLogo from '@/public/images/social/linkedin_logo.svg';
 import twitterLogo from '@/public/images/social/twitter_logo.svg';
 import facebookLogo from '@/public/images/social/facebook_logo.svg';
 import discordLogo from '@/public/images/social/discord_logo.svg';
+import Head from 'next/head';
 
 
 const profile: Profile = {
@@ -57,84 +58,90 @@ const profile: Profile = {
 
 export default function ProfilePage() {
   return (
-    <div className="relative z-0 flex-1 overflow-y-auto focus:outline-none xl:order-last">
-        <div>
-            <Image className="h-32 w-full object-cover object-top lg:h-48" src={mistyMountains} alt="Profile background" />
-        </div>
-        <div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
-            <div className="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
-                <div className="flex">
-                    <Image
-                        className="h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32"
-                        src={joshPic}
-                        alt="Josh Elias profile picture"
-                    />
-                </div>
-                <div className="mt-6 sm:flex sm:min-w-0 sm:flex-1 sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
-                    <div className="mt-6 min-w-0 flex-1 sm:hidden 2xl:block">
-                        <h1 className="truncate text-2xl font-bold text-gray-900">{profile.name}</h1>
-                    </div>
-                    <div className="justify-stretch mt-6 flex flex-col space-y-3 sm:flex-row sm:space-y-0 sm:space-x-4">
-                        <a
-                            href={`mailto: ${profile.fields.Email}`}
-                            className="inline-flex justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2"
-                        >
-                            <EnvelopeIcon className="-ml-1 mr-2 h-5 w-5 text-gray-400" aria-hidden="true" />
-                            <span>Message</span>
-                        </a>
-                        <a
-                            href={`tel: ${profile.fields.Phone}`}
-                            className="inline-flex justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2"
-                        >
-                            <PhoneIcon className="-ml-1 mr-2 h-5 w-5 text-gray-400" aria-hidden="true" />
-                            <span>Call</span>
-                        </a>
-                    </div>
-                </div>
+    <>
+        <Head>
+            <title>Profile - Daemon Web Tools</title>
+            <meta name="description" content="Profile for Josh Elias" />
+        </Head>
+        <div className="relative z-0 flex-1 overflow-y-auto focus:outline-none xl:order-last">
+            <div>
+                <Image className="h-32 w-full object-cover object-top lg:h-48" src={mistyMountains} alt="Profile background" />
             </div>
-            <div className="mt-6 hidden min-w-0 flex-1 sm:block 2xl:hidden">
-                <h1 className="truncate text-2xl font-bold text-gray-900">{profile.name}</h1>
-            </div>
-        </div>
-
-        {/* Description list */}
-        <div className="mx-auto mt-6 max-w-5xl px-4 sm:px-6 lg:px-8">
-            <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
-                {Object.keys(profile.fields).map((field) => (
-                    <div key={field} className="sm:col-span-1">
-                        <dt className="text-sm font-medium text-gray-500">{field}</dt>
-                        <dd className="mt-1 text-sm text-gray-900">{profile.fields[field as keyof ProfileFields]}</dd>
+            <div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
+                <div className="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
+                    <div className="flex">
+                        <Image
+                            className="h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32"
+                            src={joshPic}
+                            alt="Josh Elias profile picture"
+                        />
                     </div>
-                ))}
-                <div className="sm:col-span-2">
-                    <dt className="text-sm font-medium text-gray-500">Social</dt>
-                    <dd className='flex gap-3 mt-1'>
-                        {profile.social.map((social) => (
-                            <a 
-                                key={social.alt} 
-                                target="_blank" 
-                                rel="noreferrer" 
-                                href={social.url}
+                    <div className="mt-6 sm:flex sm:min-w-0 sm:flex-1 sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
+                        <div className="mt-6 min-w-0 flex-1 sm:hidden 2xl:block">
+                            <h1 className="truncate text-2xl font-bold text-gray-900">{profile.name}</h1>
+                        </div>
+                        <div className="justify-stretch mt-6 flex flex-col space-y-3 sm:flex-row sm:space-y-0 sm:space-x-4">
+                            <a
+                                href={`mailto: ${profile.fields.Email}`}
+                                className="inline-flex justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2"
                             >
-                                <Image 
-                                    src={social.icon} 
-                                    alt={social.alt} 
-                                    width={32}
-                                    height={32}
-                                />
+                                <EnvelopeIcon className="-ml-1 mr-2 h-5 w-5 text-gray-400" aria-hidden="true" />
+                                <span>Message</span>
                             </a>
-                        ))}
-                    </dd>
+                            <a
+                                href={`tel: ${profile.fields.Phone}`}
+                                className="inline-flex justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2"
+                            >
+                                <PhoneIcon className="-ml-1 mr-2 h-5 w-5 text-gray-400" aria-hidden="true" />
+                                <span>Call</span>
+                            </a>
+                        </div>
+                    </div>
                 </div>
-                <div className="sm:col-span-2">
-                    <dt className="text-sm font-medium text-gray-500">About</dt>
-                    <dd
-                        className="mt-1 max-w-prose space-y-5 text-sm text-gray-900"
-                        dangerouslySetInnerHTML={{ __html: profile.about }}
-                    />
+                <div className="mt-6 hidden min-w-0 flex-1 sm:block 2xl:hidden">
+                    <h1 className="truncate text-2xl font-bold text-gray-900">{profile.name}</h1>
                 </div>
-            </dl>
+            </div>
+
+            {/* Description list */}
+            <div className="mx-auto mt-6 max-w-5xl px-4 sm:px-6 lg:px-8">
+                <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+                    {Object.keys(profile.fields).map((field) => (
+                        <div key={field} className="sm:col-span-1">
+                            <dt className="text-sm font-medium text-gray-500">{field}</dt>
+                            <dd className="mt-1 text-sm text-gray-900">{profile.fields[field as keyof ProfileFields]}</dd>
+                        </div>
+                    ))}
+                    <div className="sm:col-span-2">
+                        <dt className="text-sm font-medium text-gray-500">Social</dt>
+                        <dd className='flex gap-3 mt-1'>
+                            {profile.social.map((social) => (
+                                <a 
+                                    key={social.alt} 
+                                    target="_blank" 
+                                    rel="noreferrer" 
+                                    href={social.url}
+                                >
+                                    <Image 
+                                        src={social.icon} 
+                                        alt={social.alt} 
+                                        width={32}
+                                        height={32}
+                                    />
+                                </a>
+                            ))}
+                        </dd>
+                    </div>
+                    <div className="sm:col-span-2">
+                        <dt className="text-sm font-medium text-gray-500">About</dt>
+                        <dd
+                            className="mt-1 max-w-prose space-y-5 text-sm text-gray-900"
+                            dangerouslySetInnerHTML={{ __html: profile.about }}
+                        />
+                    </div>
+                </dl>
+            </div>
         </div>
-    </div>
+    </>
   )
 }
