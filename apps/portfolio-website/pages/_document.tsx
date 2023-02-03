f37f1e545137d2bd83effa1cb38175b648cd41fb
Josh Elias <josh@daemontech.tools>
2023-02-03 16:32:13 -0500

    Try to add favicon

--- apps/portfolio-website/pages/_document.tsx
+++ apps/portfolio-website/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en" className='h-full bg-gray-100'>
       <Head>
-          <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       </Head>
       <body className="h-full">
         <Main />
